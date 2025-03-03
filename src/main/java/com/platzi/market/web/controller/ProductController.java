4751776b26eb6296557846ccf3967ad3177b9ad8
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2025-03-03 17:59:01 -0500

    Controlador - Metodo Post por revisar

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.platzi.market.web.controller;
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +15,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
     public List<Product> getAll(){
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId){
+    @GetMapping("/{Id}")
+    public Optional<Product> getProduct(@PathVariable("Id") int productId){
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId){
+    @GetMapping("/category/{Id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("Id") int categoryId){
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(int productId) {
+    @DeleteMapping("/delete/{Id}")
+    public boolean delete(@PathVariable("Id") int productId) {
         return productService.delete(productId);
     }
 }
